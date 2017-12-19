7583fc0226adfebebe4d580da5630cf3e71c97ba
Onur Eker <yonureker@gmail.com>
2017-12-19 18:02:43 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -2,11 +2,8 @@ def my_each(collection)
 	i = 0
 
 	while i < array.length
-		yield collection[i]
+		yield (collection[i])
 		i += 1
 	end
-end
-
-my_each(collection) do |i|
-	puts i
+	collection
 end
