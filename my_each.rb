57704faba841312e33b0afe4994258b313b382e6
Onur Eker <yonureker@gmail.com>
2017-12-19 18:00:24 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,12 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(collection)
+	i = 0
+
+	while i < array.length
+		yield collection[i]
+		i += 1
+	end
+end
+
+my_each(collection) do |i|
+	puts i
 end
